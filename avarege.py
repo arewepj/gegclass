1eb674f0d54250f5355a807a675c04a4e3155239
arewepj <152178619+arewepj@users.noreply.github.com>
2023-12-30 11:42:46 +0330

    avarege was added

---
+++ avarege.py
@@ -0,0 +1,12 @@
+x=7
+y=31
+av=(x+y)/2
+
+if av>x:
+		print("x is smaller than av")
+else :
+		print("x is bigger than av")
+if av<y:
+		print("y is bigger than av")
+else :
+		print("y is smaller than av")
--- phyton2.py
+++ phyton2.py
@@ -4,3 +4,4 @@ if y > x:
 	print("y is bigger than x")
 else:
 	print("x is bigger than y")
+
